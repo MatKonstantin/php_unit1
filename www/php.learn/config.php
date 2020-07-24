ce3ea0a33ec9293131ffa23be59be24f0266993f
Konstantin Matyashevsliy <matyashevskiy@tecman.ru>
2020-07-24 21:17:19 +0300

    lesson 2

---
+++ www/php.learn/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('AUTHOR', 'Иван Иванович');
+define('YEAR', 2020);
+
+define('DBHOST', 'localhost');
+define('DBLOGIN', 'root');
+define('DBPASS', 'root');
+define('DBNAME', 'eshop');
